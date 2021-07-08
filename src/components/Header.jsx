def15666c9eabc0b8fb6290a85bfa0f954db2560
Matias Aquino <m4tias.a@gmail.com>
2021-07-08 18:06:59 -0300

    update header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "../assets/styles/components/Header.css";
+import logo from "../assets/static/logo.png";
+import Particles from "react-particles-js";
+import Fondo from "../assets/static/fondo.svg";
 
 const Header = () => {
     return (
         <section className="header">
             <nav className="header__menu">
                 <div className="header__brand">
-                    <i className="fab fa-servicestack"></i>
-                    <h2>NEMF</h2>
+                    <img src={logo} alt="logo onetech" />
                 </div>
 
                 <div className="header__links">
@@ -23,9 +25,50 @@ const Header = () => {
                     Conectamos Hoy con el Futuro,
                     entregando siempre la milla extra.
                 </h1>
-
-                <button>Contactanos</button>
+                <i class="fas fa-arrow-down"></i>
+                <img src={Fondo} alt="website" />
             </div>
+            <Particles
+                className="header__particles"
+                options={{
+                    background: {
+                        color: {
+                            value: "#ff0000",
+                        }
+                    }
+                }}
+                params={{
+                    "particles": {
+                        "number": {
+                            "value": 50
+                        },
+                        "size": {
+                            "value": 3
+                        },
+                        "color": {
+                            "value": "#000000"
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                          enable: true,
+                        }
+                    },
+                    detectRetina: true,
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        }
+                    }
+                }} />
         </section>
     )
 };
