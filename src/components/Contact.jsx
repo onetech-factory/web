4eae378d99ab73cd5240fd964fb70d49a80802e4
Matias Aquino <m4tias.a@gmail.com>
2021-07-10 15:03:54 -0300

    create contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import "../assets/styles/components/Contact.css";
 
-const Contact = () => (
-    <section className="contact">
-        <h2 className="contact__title">Contacto</h2>
-        <form action="/" className="contact__form">
-            <p>Envianos tu consulta</p>
-            <div className="contact__inputs">
-                <input type="text" placeholde="Nombre" />
-                <input type="text" placeholde="Asunto" />
-            </div>
-            <input type="textarea" placeholder="Mensaje" />
-            <button>Enviar</button>
-        </form>
-    </section>
-);
+const Contact = () => {
+
+    useEffect(() => {
+        Aos.init({duration: 2000});
+    }, []);
+
+    return (
+        <section className="contact" data-aos="fade-up">
+            <h2 className="contact__title">Contacto</h2>
+            <form action="/" className="contact__form">
+                <p>Envianos tu consulta</p>
+                <div className="contact__inputs">
+                    <input type="text" placeholder="Nombre" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="text" placeholder="Asunto" />
+                </div>
+                <textarea name="message" className="contact__textarea" placeholder="Mensaje"></textarea>
+                <button>Enviar</button>
+            </form>
+        </section>
+    )
+};
 
 export default Contact;
