a1fed0dfd1d1425f6c2ea70e9f62223c2d50b9eb
Matias Aquino <m4tias.a@gmail.com>
2021-07-24 22:24:10 -0300

    remove formik, yup in form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,9 @@
 import React, { useEffect } from "react";
 import Aos from "aos";
-import Formik from "formik";
-import ContactSchema from "./Yup"; 
 import "aos/dist/aos.css";
 import "../assets/styles/components/Contact.css";
 
-const Contact = (props) => {
-
-    // FORMIK VALUES
-    const {
-        values, 
-        touched,
-        errors,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting
-    } = props;
+const Contact = () => {
 
     useEffect(() => {
         Aos.init({ duration: 2000 });
@@ -26,36 +13,28 @@ const Contact = (props) => {
         <section className="contact" data-aos="fade-up" id="contact">
             <h2 className="contact__title">Contacto</h2>
             <span className="separator">•</span>
-            <form className="contact__form" onSubmit={handleSubmit}>
+            <form className="contact__form">
                 <p>Envianos tu consulta y nos pondremos en contacto!</p>
                 <div className="contact__inputs">
                     <input
-                        type="nombre"
+                        type="text"
                         placeholder="Nombre"
                         name="nombre"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
                     />
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <input
                         type="email"
                         placeholder="Correo electrónico"
                         name="email"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
                     />
                 </div>
                 <textarea
                     name="mensaje"
                     className="contact__textarea"
                     placeholder="Mensaje"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
                 >
                 </textarea>
-                <button type="submit" disabled={isSubmitting}>
-                    {isSubmitting ? "Submitting" : "Submit"}
-                </button>
+                <button type="submit">Enviar</button>
             </form>
         </section>
     )
