15c1a2c221b9b206d07b4d65869e7015a7d91574
Matias Aquino <m4tias.a@gmail.com>
2021-08-17 05:28:05 -0300

    add emailjs and sweet alert in contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,27 @@
 import React, { useEffect } from "react";
 import Aos from "aos";
-import { Formik, Field } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
+import emailjs, { init } from 'emailjs-com';
+import Swal from 'sweetalert2';
 import "aos/dist/aos.css";
 import "../assets/styles/components/Contact.css";
 
+init("user_ItTEUboVgoO9qq6U1wSsJ");
+
 const contactSchema = Yup.object().shape({
     nombre: Yup.string()
-        .min(2, "Mínimo 2 caracteres")
-        .max(50, "Máximo 50 caracteres")
-        .required("Campo obligatorio"),
-    email: Yup.string()
-        .email("Correo invalido")
-        .required("Campo obligatorio"),
+        .min(2, "Nombre muy corto")
+        .max(50, "Nombre: Maximo 50 caracteres")
+        .required("Nombre Requerido"),
+    correo: Yup.string()
+        .email("Correo Invalido")
+        .required("Correo Requerido"),
     mensaje: Yup.string()
-        .min(10, "Mínimo 10 caracteres")
-        .max(400, "Máximo 50 caracteres")
-        .required("Campo obligatorio")
+        .min(10, "Mensaje muy corto")
+        .max(400, "Mensaje: Maximo 400 caracteres")
+        .required("Mensaje Requerido")
 });
-
-const handleClick = (e) => {
-    e.preventDefault();
-    console.log("Se detuvo el envio");
-}
 
 const Contact = () => {
 
@@ -30,56 +29,73 @@ const Contact = () => {
         Aos.init({ duration: 2000 });
     }, []);
 
+    const formik = useFormik({
+        initialValues: { nombre: "", correo: "", mensaje: "", },
+        validationSchema: contactSchema,
+        onSubmit: (values, { setSubmitting, resetForm }) => {
+            emailjs.send('gmail', 'template_nuyel5o', values)
+                .then(function () {
+                    console.log('SUCCESS!');
+                    Swal.fire({
+                        title: "Mensaje enviado!",
+                        text: "Pronto nos pondremos en contacto",
+                        icon: "success",
+                        confirmButtonColor: "#fca311"
+                    })
+                    setSubmitting(false);
+                    resetForm();
+                }, function (error) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Algo salio mal!',
+                        confirmButtonColor: "#fca311"
+                    })
+                    setSubmitting(false);
+                    resetForm();
+                });
+        }
+    })
+
     return (
         <section className="contact" data-aos="fade-up" id="contact">
             <h2 className="contact__title">Contacto</h2>
             <span className="separator">•</span>
 
-            <Formik
-                initialValues={{ nombre: "", email: "", mensaje: "" }}
-                validationSchema={contactSchema}
-                onSubmit={(values) => {
-                    console.log(values);
-                }}
-            >
-                {
-                    ({
-                        values,
-                        errors,
-                        touched,
-                        isSubmitting
-                    }) => (
-                        <form className="contact__form">
-                            <p>Envianos tu consulta y nos pondremos en contacto!</p>
-                            <div className="contact__inputs">
-                                <Field name="nombre" input="input" placeholder="Nombre" />
-                                {
-                                    errors.nombre && touched.nombre ? (
-                                        <span>{errors.name}</span>
-                                    ) : null
-                                }
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <Field name="email" input="input" placeholder="Correo electronico" />
-                                {
-                                    errors.email && touched.email ? (
-                                        <small>{errors.email}</small>
-                                    ) : null
-                                }
-                            </div>
-                            <Field name="mensaje" input="textarea" placeholder="Mensaje" className="contact__textarea" />
-                            {
-                                errors.mensaje && touched.mensaje ? (
-                                    <small>{errors.mensaje}</small>
-                                ) : null
-                            }
-                            <small>{errors.mensaje}</small>
-                            <button type="submit" disabled={isSubmitting} onClick={handleClick}>
-                                Enviar
-                            </button>
-                        </form>
-                    )
-                }
-            </Formik>
+            <form onSubmit={formik.handleSubmit} className="contact__form">
+                <div className="contact__inputs">
+                    <input
+                        name="nombre"
+                        type="text"
+                        placeholder="Nombre"
+                        onChange={formik.handleChange}
+                        value={formik.values.nombre}
+                        className={formik.errors.nombre && formik.touched.nombre && "input-invalid"}
+                    />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input
+                        name="correo"
+                        type="text"
+                        placeholder="Correo electronico"
+                        onChange={formik.handleChange}
+                        value={formik.values.correo}
+                        className={formik.errors.correo && formik.touched.correo && "input-invalid"}
+                    />
+                </div>
+                <textarea
+                    name="mensaje"
+                    placeholder="Mensaje"
+                    onChange={formik.handleChange}
+                    value={formik.values.mensaje}
+                    className={formik.errors.mensaje && formik.touched.mensaje ? "contact__textarea input-invalid" : "contact__textarea"}
+                />
+                <div className="contact__errors">
+                    {formik.touched.nombre && formik.errors.nombre && <div>{formik.errors.nombre}</div>}
+                    {formik.touched.correo && formik.errors.correo && <div>{formik.errors.correo}</div>}
+                    {formik.touched.mensaje && formik.errors.mensaje && <div>{formik.errors.mensaje}</div>}
+                </div>
+                <button type="submit" disabled={formik.isSubmitting}>Enviar</button>
+            </form>
         </section>
     )
 };
